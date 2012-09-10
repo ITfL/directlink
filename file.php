444a646bc65a5ac76c487ca959cf74516737d165
hans-christian <hans.sperker@gmail.com>
2012-09-10 11:27:08 +0200

    Changed MIME-TYPE function to use mimeinfo ...

--- file.php
+++ file.php
@@ -69,7 +69,8 @@ if(shared_file_exists($filename, $instance_dl_data->connection_id)) {
 		header( 'Expires: 0' );
 		header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
 		header( 'Cache-Control: private', false ); // required for certain browsers
-		header( 'Content-Type: '.mime_content_type( $filename ) );
+		// header( 'Content-Type: '.mime_content_type( $filename ) );
+		header( 'Content-Type: '. mimeinfo('type', $filename) );
 		header( 'Content-Disposition: attachment; filename="'. basename( $filename ) . '";' );
 		header( 'Content-Transfer-Encoding: binary' );
 		header( 'Content-Length: ' . filesize( $filename ) );
