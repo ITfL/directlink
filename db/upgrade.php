16c81827d8c2e1dd72be237fb3cb22f89074d9b3
Sebastian Boosz <sebastian.boosz@googlemail.com>
2014-08-04 13:21:22 +0200

    Added field for embedding in database

--- db/upgrade.php
+++ db/upgrade.php
@@ -84,6 +84,20 @@ function xmldb_directlink_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2012041801, 'directlink');
     }
 
+    if ($oldversion < 2014080401){
+        // Define field id to be added to directlink.
+        $table = new xmldb_table('directlink');
+        $field = new xmldb_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
+
+        // Conditionally launch add field id.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Directlink savepoint reached.
+        upgrade_mod_savepoint(true, 2014080401, 'directlink');
+    }
+
 
 /**
  * Not needed yet
